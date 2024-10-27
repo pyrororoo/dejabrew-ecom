05a6b3c8c22ad3b2bbc409cc02c9b2bfaee009e7
Pororo <157348093+pyrororoo@users.noreply.github.com>
2024-10-27 16:28:14 +0800

    Modal content finished

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
-const addCartBtn = document.getElementsByClassName("addCartBtn")
-const addCartSuccess = document.getElementById('addCartSuccess')
+const addCartBtn = document.getElementsByClassName("addCartBtn");
+const addCartSuccess = document.getElementById('addCartSuccess');
 
 for (var i = 0; i < addCartBtn.length; i++) {
 
   if (addCartBtn[i]) {
-    const toastBootstrap = bootstrap.Toast.getOrCreateInstance(addCartSuccess)
+    const toastBootstrap = bootstrap.Toast.getOrCreateInstance(addCartSuccess);
     addCartBtn[i].addEventListener('click', () => {
-      toastBootstrap.show()
+      toastBootstrap.show();
     })
   }
 
